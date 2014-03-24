b466586e78c30d903d6d024b9ddf7921b3372874
ponkotuy <web@ponkotuy.com>
2014-03-24 09:36:00 +0900

    Fix ResType Regexp

--- client/src/main/scala/com/ponkotuy/parser/ResType.scala
+++ client/src/main/scala/com/ponkotuy/parser/ResType.scala
@@ -28,44 +28,44 @@ object ResType extends Log {
   val ReqQuest = s"$Api/api_req_quest"
   val ReqNyukyo = s"$Api/api_req_nyukyo"
 
-  case object LoginCheck extends ResType(s"$AuthMember/logincheck".r) // 取るべきではない
-  case object Material extends ResType(s"$GetMember/material".r)
-  case object Basic extends ResType(s"$GetMember/basic".r)
-  case object Record extends ResType(s"$GetMember/record".r) // Basicの綺麗版
-  case object Ship2 extends ResType(s"$GetMember/ship2".r) // どうせship3に含まれているんだろう？
-  case object Ship3 extends ResType(s"$GetMember/ship3".r)
-  case object NDock extends ResType(s"$GetMember/ndock".r)
-  case object KDock extends ResType(s"$GetMember/kdock".r)
-  case object Deck extends ResType(s"$GetMember/deck".r) // DeckPortと何が違うのか分からなくて困っている
-  case object DeckPort extends ResType(s"$GetMember/deck_port".r)
-  case object Practice extends ResType(s"$GetMember/practice".r) // 演習相手。取るべきではない
-  case object Book2 extends ResType(s"$GetMember/book2".r)
-  case object CreateShip extends ResType(s"$ReqKousyou/createship".r)
-  case object GetShip extends ResType(s"$ReqKousyou/getship".r) // IDとshipIDのみ
-  case object CreateItem extends ResType(s"$ReqKousyou/createitem".r)
-  case object Charge extends ResType(s"$ReqHokyu/charge".r) // 特に要らない
-  case object HenseiChange extends ResType(s"$ReqHensei/change".r) // 特に要らない
-  case object HenseiLock extends ResType(s"$ReqHensei/lock".r)
-  case object MissionStart extends ResType(s"$ReqMission/start".r) // 特に要らない
-  case object KaisouPowerup extends ResType(s"$ReqKaisou/powerup".r)
-  case object PracticeBattle extends ResType(s"$ReqPractice/battle".r)
-  case object PracticeBattleResult extends ResType(s"$ReqPractice/battle_result".r)
-  case object GetOthersDeck extends ResType(s"$ReqMember/getothersdeck".r) // 演習相手。取るべきではない
-  case object MapStart extends ResType(s"$ReqMap/start".r)
-  case object MapNext extends ResType(s"$ReqMap/next".r)
-  case object SortieBattle extends ResType(s"$ReqSortie/battle".r)
-  case object SortieBattleResult extends ResType(s"$ReqSortie/battleresult".r)
-  case object ClearItemGet extends ResType(s"$ReqQuest/clearitemget".r)
-  case object NyukyoStart extends ResType(s"$ReqNyukyo/start".r)
-  case object MasterShip extends ResType(s"$GetMaster/ship".r)
-  case object MasterPractice extends ResType(s"$GetMaster/practice".r)
-  case object MasterUseItem extends ResType(s"$GetMaster/useitem".r) // 高速修復材とかの説明
-  case object MasterFurniture extends ResType(s"$GetMaster/furniture".r) // 家具の説明
-  case object MasterSlotItem extends ResType(s"$GetMaster/slotitem".r)
-  case object MasterMapArea extends ResType(s"$GetMaster/maparea".r) // 鎮守府海域・南西諸島海域など
-  case object MasterSType extends ResType(s"$GetMaster/stype".r)
-  case object MasterMission extends ResType(s"$GetMaster/mission".r)
-  case object ShipSWF extends ResType("""/kcs/ships/[0-9]+\.swf""".r)
+  case object LoginCheck extends ResType(s"\\A$AuthMember/logincheck\\z".r) // 取るべきではない
+  case object Material extends ResType(s"\\A$GetMember/material\\z".r)
+  case object Basic extends ResType(s"\\A$GetMember/basic\\z".r)
+  case object Record extends ResType(s"\\A$GetMember/record\\z".r) // Basicの綺麗版
+  case object Ship2 extends ResType(s"\\A$GetMember/ship2\\z".r) // どうせship3に含まれているんだろう？
+  case object Ship3 extends ResType(s"\\A$GetMember/ship3\\z".r)
+  case object NDock extends ResType(s"\\A$GetMember/ndock\\z".r)
+  case object KDock extends ResType(s"\\A$GetMember/kdock\\z".r)
+  case object Deck extends ResType(s"\\A$GetMember/deck\\z".r) // DeckPortと何が違うのか分からなくて困っている
+  case object DeckPort extends ResType(s"\\A$GetMember/deck_port\\z".r)
+  case object Practice extends ResType(s"\\A$GetMember/practice\\z".r) // 演習相手。取るべきではない
+  case object Book2 extends ResType(s"\\A$GetMember/book2\\z".r)
+  case object CreateShip extends ResType(s"\\A$ReqKousyou/createship\\z".r)
+  case object GetShip extends ResType(s"\\A$ReqKousyou/getship\\z".r) // IDとshipIDのみ
+  case object CreateItem extends ResType(s"\\A$ReqKousyou/createitem\\z".r)
+  case object Charge extends ResType(s"\\A$ReqHokyu/charge\\z".r) // 特に要らない
+  case object HenseiChange extends ResType(s"\\A$ReqHensei/change\\z".r) // 特に要らない
+  case object HenseiLock extends ResType(s"\\A$ReqHensei/lock\\z".r)
+  case object MissionStart extends ResType(s"\\A$ReqMission/start\\z".r) // 特に要らない
+  case object KaisouPowerup extends ResType(s"\\A$ReqKaisou/powerup\\z".r)
+  case object PracticeBattle extends ResType(s"\\A$ReqPractice/battle\\z".r)
+  case object PracticeBattleResult extends ResType(s"\\A$ReqPractice/battle_result\\z".r)
+  case object GetOthersDeck extends ResType(s"\\A$ReqMember/getothersdeck\\z".r) // 演習相手。取るべきではない
+  case object MapStart extends ResType(s"\\A$ReqMap/start\\z".r)
+  case object MapNext extends ResType(s"\\A$ReqMap/next\\z".r)
+  case object SortieBattle extends ResType(s"\\A$ReqSortie/battle\\z".r)
+  case object SortieBattleResult extends ResType(s"\\A$ReqSortie/battleresult\\z".r)
+  case object ClearItemGet extends ResType(s"\\A$ReqQuest/clearitemget\\z".r)
+  case object NyukyoStart extends ResType(s"\\A$ReqNyukyo/start\\z".r)
+  case object MasterShip extends ResType(s"\\A$GetMaster/ship\\z".r)
+  case object MasterPractice extends ResType(s"\\A$GetMaster/practice\\z".r)
+  case object MasterUseItem extends ResType(s"\\A$GetMaster/useitem\\z".r) // 高速修復材とかの説明
+  case object MasterFurniture extends ResType(s"\\A$GetMaster/furniture\\z".r) // 家具の説明
+  case object MasterSlotItem extends ResType(s"\\A$GetMaster/slotitem\\z".r)
+  case object MasterMapArea extends ResType(s"\\A$GetMaster/maparea\\z".r) // 鎮守府海域・南西諸島海域など
+  case object MasterSType extends ResType(s"\\A$GetMaster/stype\\z".r)
+  case object MasterMission extends ResType(s"\\A$GetMaster/mission\\z".r)
+  case object ShipSWF extends ResType("""\A/kcs/ships/[0-9]+\.swf\z""".r)
 
   val values = Set(
     LoginCheck, Material, Basic, Record, Ship2, Ship3, NDock, KDock,
