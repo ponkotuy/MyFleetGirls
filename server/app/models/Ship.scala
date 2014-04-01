7263e46f1275809da1ea35b29500dfa5add337ef
ponkotuy <web@ponkotuy.com>
2014-04-02 00:53:00 +0900

    Add Armed Ships View

--- server/app/models/Ship.scala
+++ server/app/models/Ship.scala
@@ -19,36 +19,43 @@ case class Ship(
     created: Long)
 
 object Ship extends SQLSyntaxSupport[Ship] {
-  override val columnNames = Seq("id", "ship_id", "member_id", "lv", "exp", "nowhp", "slot", "fuel", "bull", "dock_time", "cond",
+  override val columnNames = Seq("id", "ship_id", "member_id", "lv", "exp", "nowhp", "fuel", "bull", "dock_time", "cond",
     "karyoku", "raisou", "taiku", "soukou", "kaihi", "taisen", "sakuteki", "lucky", "locked", "created")
-  def apply(x: SyntaxProvider[Ship])(rs: WrappedResultSet): Ship = apply(x.resultName)(rs)
-  def apply(x: ResultName[Ship])(rs: WrappedResultSet): Ship = new Ship(
-    rs.int(x.id),
-    rs.int(x.shipId),
-    rs.long(x.memberId),
-    rs.int(x.lv),
-    rs.int(x.exp),
-    rs.int(x.nowhp),
-    rs.string(x.slot).split(',').map(_.toInt).toList,
-    rs.int(x.fuel),
-    rs.int(x.bull),
-    rs.long(x.dockTime),
-    rs.int(x.cond),
-    rs.int(x.karyoku),
-    rs.int(x.raisou),
-    rs.int(x.taiku),
-    rs.int(x.soukou),
-    rs.int(x.kaihi),
-    rs.int(x.taisen),
-    rs.int(x.sakuteki),
-    rs.int(x.lucky),
-    rs.boolean(x.locked),
-    rs.long(x.created)
-  )
+  def apply(x: SyntaxProvider[Ship])(rs: WrappedResultSet): Ship =
+    apply(x.resultName)(rs)
+  def apply(x: ResultName[Ship])(rs: WrappedResultSet): Ship = {
+    val id = rs.int(x.id)
+    val memberId = rs.long(x.memberId)
+    val slot = findSlot(memberId, id)
+    new Ship(
+      id,
+      rs.int(x.shipId),
+      memberId,
+      rs.int(x.lv),
+      rs.int(x.exp),
+      rs.int(x.nowhp),
+      slot,
+      rs.int(x.fuel),
+      rs.int(x.bull),
+      rs.long(x.dockTime),
+      rs.int(x.cond),
+      rs.int(x.karyoku),
+      rs.int(x.raisou),
+      rs.int(x.taiku),
+      rs.int(x.soukou),
+      rs.int(x.kaihi),
+      rs.int(x.taisen),
+      rs.int(x.sakuteki),
+      rs.int(x.lucky),
+      rs.boolean(x.locked),
+      rs.long(x.created)
+    )
+  }
 
   lazy val s = Ship.syntax("s")
   lazy val ms = MasterShip.syntax("ms")
   lazy val ds = DeckShip.syntax("ds")
+  lazy val ssi = ShipSlotItem.syntax("ssi")
 
   def findByUserMaxLvWithName(memberId: Long)(implicit session: DBSession = autoSession): Option[ShipWithName] = {
     withSQL {
@@ -64,8 +71,23 @@ object Ship extends SQLSyntaxSupport[Ship] {
       select.from(Ship as s)
         .leftJoin(MasterShip as ms).on(s.shipId, ms.id)
         .where.eq(s.memberId, memberId).and.eq(s.id, sid)
-    }.map { rs => ShipWithName(Ship(s)(rs), MasterShip(ms)(rs)) }.first().apply()
+    }.map { rs =>
+      ShipWithName(Ship(s)(rs), MasterShip(ms)(rs))
+    }.first().apply()
   }
+
+  def findIn(memberId: Long, ids: Seq[Int])(implicit session: DBSession = autoSession): List[ShipWithName] =
+    ids match {
+      case Seq() => Nil
+      case _ =>
+        withSQL {
+          select.from(Ship as s)
+            .leftJoin(MasterShip as ms).on(s.shipId, ms.id)
+            .where.eq(s.memberId, memberId).and.in(s.id, ids)
+        }.map { rs =>
+          ShipWithName(Ship(s)(rs), MasterShip(ms)(rs))
+        }.toList().apply()
+    }
 
   def findAllByUser(memberId: Long)(implicit session: DBSession = Ship.autoSession): List[Ship] = withSQL {
     select.from(Ship as s)
@@ -80,13 +102,16 @@ object Ship extends SQLSyntaxSupport[Ship] {
     }.map { rs => ShipWithName(Ship(s)(rs), MasterShip(ms)(rs)) }.toList().apply()
   }
 
+  def findSlot(memberId: Long, shipId: Int): List[Int] =
+    ShipSlotItem.findAllBy(sqls"member_id = ${memberId} and ship_id = ${shipId}").map(_.slotitemId)
+
   def create(s: data.Ship, memberId: Long)(implicit session: DBSession = Ship.autoSession): Ship = {
     val created = System.currentTimeMillis()
-    val slot = s.slot.mkString(",")
+    ShipSlotItem.bulkInsert(s.slot, memberId, s.id)
     withSQL {
       insert.into(Ship).namedValues(
         column.id -> s.id, column.shipId -> s.shipId, column.memberId -> memberId,
-        column.lv -> s.lv, column.exp -> s.exp, column.nowhp -> s.nowhp, column.slot -> slot,
+        column.lv -> s.lv, column.exp -> s.exp, column.nowhp -> s.nowhp,
         column.fuel -> s.fuel, column.bull -> s.bull, column.dockTime -> s.dockTime, column.cond -> s.cond,
         column.karyoku -> s.karyoku, column.raisou -> s.raisou, column.taiku -> s.taiku, column.soukou -> s.soukou,
         column.kaihi -> s.kaihi, column.taisen -> s.taisen, column.sakuteki -> s.sakuteki, column.lucky -> s.lucky,
@@ -101,18 +126,18 @@ object Ship extends SQLSyntaxSupport[Ship] {
   def bulkInsert(ss: Seq[data.Ship], memberId: Long)(
       implicit session: DBSession = Ship.autoSession): Seq[Ship] = {
     val created = System.currentTimeMillis()
-    val slots = ss.map(_.slot.mkString(","))
+    ShipSlotItem.bulkInserts(ss.map(_.slot), memberId, ss.map(_.id))
     applyUpdate {
       insert.into(Ship).columns(
         column.id, column.shipId, column.memberId,
-        column.lv, column.exp, column.nowhp, column.slot,
+        column.lv, column.exp, column.nowhp,
         column.fuel, column.bull, column.dockTime, column.cond,
         column.karyoku, column.raisou, column.taiku, column.soukou,
         column.kaihi, column.taisen, column.sakuteki, column.lucky,
         column.locked, column.created
       ).multiValues(
           ss.map(_.id), ss.map(_.shipId), Seq.fill(ss.size)(memberId),
-          ss.map(_.lv), ss.map(_.exp), ss.map(_.nowhp), slots,
+          ss.map(_.lv), ss.map(_.exp), ss.map(_.nowhp),
           ss.map(_.fuel), ss.map(_.bull), ss.map(_.dockTime), ss.map(_.cond),
           ss.map(_.karyoku), ss.map(_.raisou), ss.map(_.taiku), ss.map(_.soukou),
           ss.map(_.kaihi), ss.map(_.taisen), ss.map(_.sakuteki), ss.map(_.lucky),
@@ -128,8 +153,18 @@ object Ship extends SQLSyntaxSupport[Ship] {
     }
   }
 
-  def deleteAllByUser(memberId: Long)(implicit session: DBSession = Ship.autoSession): Unit =
+  def destroy(memberId: Long, id: Int)(implicit session: DBSession = autoSession): Unit = {
+    ShipSlotItem.deleteAllBy(sqls"member_id = ${memberId} and ship_id = ${s.id}")
+    applyUpdate {
+      delete.from(Ship)
+        .where.eq(Ship.column.memberId, memberId).and.eq(Ship.column.id, id)
+    }
+  }
+
+  def deleteAllByUser(memberId: Long)(implicit session: DBSession = Ship.autoSession): Unit = {
+    ShipSlotItem.deleteAllBy(sqls"member_id = ${memberId}")
     applyUpdate { delete.from(Ship).where.eq(Ship.column.memberId, memberId) }
+  }
 }
 
 case class ShipWithName(ship: Ship, master: MasterShip) {
