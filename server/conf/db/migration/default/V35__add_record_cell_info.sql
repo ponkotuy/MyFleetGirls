f6dfb745f375d376675f30cadc09f46fa227a793
ponkotuy <web@ponkotuy.com>
2014-06-07 23:08:19 +0900

    Fix Non Fleet Bug

---
+++ server/conf/db/migration/default/V35__add_record_cell_info.sql
@@ -0,0 +1,13 @@
+
+insert into cell_info values
+    (2, 4, 7, 'E', false, false),
+    (2, 5, 8, 'H', false, false),
+    (2, 5, 10, 'J', false, false),
+    (4, 3, 18, 'D', false, false),
+    (4, 4, 7, 'G', false, false),
+    (4, 4, 8, 'J', false, false),
+    (4, 4, 9, 'E', false, false),
+    (4, 4, 11, 'C', false, false),
+    (4, 4, 14, 'J', false, false),
+    (5, 5, 1, 'B', true, false),
+    (5, 5, 6, 'K', false, false);
