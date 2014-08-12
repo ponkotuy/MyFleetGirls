b1a7916a8d301f52b065184f0edb814f3fafe6e5
ponkotuy <ponkotuy@geishatokyo.com>
2014-08-13 00:53:02 +0900

    Add Record Cell Info

---
+++ server/conf/db/migration/default/V42__add_record_cell_info.sql
@@ -0,0 +1,13 @@
+
+insert into cell_info values
+    (27, 6, 3, 'C', false, false),
+    (27, 6, 4, 'D', false, false),
+    (27, 6, 5, 'E', false, false),
+    (27, 6, 6, 'F', false, false),
+    (27, 6, 7, 'G', false, false),
+    (27, 6, 8, 'H', false, false),
+    (27, 6, 9, 'I', false, false),
+    (27, 6, 10, 'J', false, true),
+    (27, 6, 11, 'F', false, false),
+    (27, 6, 12, 'G', false, false),
+    (27, 6, 14, 'J', false, true);
