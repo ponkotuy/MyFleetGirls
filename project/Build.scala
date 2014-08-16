9214ae77463515792f269019a092b9ea200ad57e
ponkotuy <web@ponkotuy.com>
2014-08-16 20:04:16 +0900

    Ver0.16.13

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import play._
 
 object MyFleetGirlsBuild extends Build {
 
-  val ver = "0.16.12"
+  val ver = "0.16.13"
 
   lazy val root = Project(id = "my-fleet-girls", base = file("."), settings = rootSettings)
     .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
