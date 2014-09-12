1d27333d09d345371895e616db02793bdeb7ae86
ponkotuy <web@ponkotuy.com>
2014-09-12 23:26:08 +0900

    Add 3-5 Cell Info

---
+++ server/conf/db/migration/default/V43__add_record_cell_info.sql
@@ -0,0 +1,17 @@
+
+insert into cell_info values
+    (3, 5, 1, 'A', true, false),
+    (3, 5, 3, 'D', false, false),
+    (3, 5, 4, 'C', false, false),
+    (3, 5, 6, 'B', true, false),
+    (3, 5, 7, 'G', false, false),
+    (3, 5, 8, 'F', false, false),
+    (3, 5, 9, 'I', false, false),
+    (3, 5, 10, 'J', false, false),
+    (3, 5, 11, 'K', false, true),
+    (3, 5, 12, 'E', false, false),
+    (3, 5, 13, 'B', false, false),
+    (3, 5, 14, 'F', false, false),
+    (3, 5, 15, 'K', false, true),
+    (27, 2, 9, 'H', false, false),
+    (27, 6, 13, 'F', false, false);
