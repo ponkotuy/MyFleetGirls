2e6db450c644fbff91c938efbb4368d0f894dca0
b-wind <perl.mania@gmail.com>
2014-11-06 22:23:56 +0900

    fix typo

--- package/resources/MyFleetGirls.bat
+++ package/resources/MyFleetGirls.bat
@@ -1,23 +1,25 @@
 @echo off
 title MyFleetGirls
-cd /d %~dp0
+SET MFG_HOME=%~dp0
+If Defined b2eprogrampathname set MFG_HOME=%b2eprogrampathname%
+cd /d %MGF_HOME%
 
 REM 環境変数 JAVA_HOME または MFG_JAVA_HOME が設定されていれば PATH に追加設定
-if Defined JAVA_HOME path %JAVA_HOME%\bin\;%HOME%
-if Defined MFG_JAVA_HOME path %MYFFLEETGIRSL_JAVA_HOME%\bin\;%HOME%
+If Defined JAVA_HOME path %JAVA_HOME%\bin\;%PATH%
+If Defined MFG_JAVA_HOME path %MFG_JAVA_HOME%\bin\;%PATH%
 
 REM java コマンドを実行し存在確認
-java -version 1> NUL 2> NUL
-if ErrorLevel 1 goto JavaError
+java.exe -version 2> NUL
+If ErrorLevel 1 goto JavaError
 
 REM update.jar で関連ファイルを更新後、MyFleetGirls.jar ( clinet 本体 ) を実行
-java -jar update.jar
-java -jar MyFleetGirls.jar
-echo "MyFleetGirls を終了します"
+java.exe -jar update.jar
+java.exe -jar MyFleetGirls.jar
+echo MyFleetGirls を終了します
 pause > NUL
 EXIT 0
 
 :JavaError
-echo "JAVA ランタイム見つかりませんでした。README.txt を参照し、適切に導入するか環境変数の設定を行ってください。"
+echo JAVA ランタイムが見つかりませんでした。README.txt を参照し導入するか、環境変数の設定を行ってください。
 pause > NUL
 EXIT 1
