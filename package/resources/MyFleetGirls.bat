47cdb3b3c77451a0e18ff6a1313614c4957e40a1
b-wind <perl.mania@gmail.com>
2014-11-06 18:40:51 +0900

    起動用バッチファイル内で java コマンドのチェック行うように変更
    環境変数の設定で Java ランタイムを指定出来るように。
    ランタイムが見つからない場合はメッセージを出力するように変更。

--- package/resources/MyFleetGirls.bat
+++ package/resources/MyFleetGirls.bat
@@ -1,7 +1,23 @@
-
 @echo off
 title MyFleetGirls
 cd /d %~dp0
+
+REM 環境変数 JAVA_HOME または MFG_JAVA_HOME が設定されていれば PATH に追加設定
+if Defined JAVA_HOME path %JAVA_HOME%\bin\;%HOME%
+if Defined MFG_JAVA_HOME path %MYFFLEETGIRSL_JAVA_HOME%\bin\;%HOME%
+
+REM java コマンドを実行し存在確認
+java -version 1> NUL 2> NUL
+if ErrorLevel 1 goto JavaError
+
+REM update.jar で関連ファイルを更新後、MyFleetGirls.jar ( clinet 本体 ) を実行
 java -jar update.jar
 java -jar MyFleetGirls.jar
-pause
+echo "MyFleetGirls を終了します"
+pause > NUL
+EXIT 0
+
+:JavaError
+echo "JAVA ランタイム見つかりませんでした。README.txt を参照し、適切に導入するか環境変数の設定を行ってください。"
+pause > NUL
+EXIT 1
