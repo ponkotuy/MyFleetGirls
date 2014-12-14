93770a8942e392723fe51ef1780fab909464814b
ponkotuy <web@ponkotuy.com>
2014-12-15 07:23:18 +0900

    Add Setup Detail Page(Only Text)

--- server/app/controllers/ViewAbout.scala
+++ server/app/controllers/ViewAbout.scala
@@ -11,4 +11,5 @@ object ViewAbout extends Controller {
   def setup = Common.actionAsync { Ok(views.html.about.setup()) }
   def changeLog = Common.actionAsync { Ok(views.html.about.change_log()) }
   def faq = Common.actionAsync { Ok(views.html.about.faq()) }
+  def setupDetail = Common.actionAsync { Ok(views.html.about.setup_detail())}
 }
