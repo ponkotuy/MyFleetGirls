b39fbf0cff1911f8eadd1411bce8daa4bf929442
ponkotuy <web@ponkotuy.com>
2015-02-25 02:12:56 +0900

    Ver1.3.2

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import play._
 
 object MyFleetGirlsBuild extends Build {
 
-  val ver = "1.3.1"
+  val ver = "1.3.2"
   val scalaVer = "2.11.5"
 
   lazy val root = Project(id = "my-fleet-girls", base = file("."), settings = rootSettings)
