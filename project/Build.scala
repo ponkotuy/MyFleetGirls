5b1c52b077f2d10b9c356b728ee22e0241853126
b-wind <perl.mania@gmail.com>
2015-03-09 00:54:16 +0900

    gzip 済み jar ファイルを用意するように

--- project/Build.scala
+++ project/Build.scala
@@ -103,6 +103,7 @@ object MyFleetGirlsBuild extends Build {
     p.Process("""rm server/public/zip/MyFleetGirls.zip""").run()
     p.Process("""zip -j server/public/zip/MyFleetGirls.zip update/target/update.jar LICENSE update/update.properties package/resources/application.conf.sample package/resources/MyFleetGirls.bat package/resources/MyFleetGirls.sh package/resources/MyFleetGirls.command package/resources/IE_PROXY.REG""").run()
     p.Process("""cp client/target/scala-2.11/MyFleetGirls.jar LICENSE MyFleetGirls.bat MyFleetGirls.sh MyFleetGirls.command package/resources/application.conf.sample package/resources/IE_PROXY.REG server/public/client/""").run()
+    p.Process("""cd server/public/client && gzip -9 -c MyFleetGirls.jar > MyFleetGirls.jar.gz""").run()
     Thread.sleep(1000L)
     state
   }
