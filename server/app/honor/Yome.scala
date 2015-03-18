022e588b8a5d5e98c62965f8cee272be846290ce
ponkotuy <web@ponkotuy.com>
2015-03-18 22:50:14 +0900

    #171

--- server/app/honor/Yome.scala
+++ server/app/honor/Yome.scala
@@ -21,7 +21,8 @@ object Yome extends HonorCategory {
     val married: Set[Int] = Ship.findAllByUser(memberId)
         .filter(100 <= _.lv)
         .map { s => EvolutionBase(s.shipId) }(breakOut)
-    val ids = yomes & married
+    val maxLv = Ship.findByUserMaxLvWithName(memberId).map(s => EvolutionBase(s.shipId))
+    val ids = yomes & (married ++ maxLv)
     val result = MasterShipBase.findAllBy(sqls.in(MasterShipBase.column.id, ids.toSeq))
     result.map(toHonor)
   }
