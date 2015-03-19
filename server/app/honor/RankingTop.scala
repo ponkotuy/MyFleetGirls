a60d6f9f43a2ee9ca3758b06f4b21fb57f1efd8d
kxbmap <kxbmap@gmail.com>
2015-03-19 22:55:50 +0900

    Fix exception with empty ranking

--- server/app/honor/RankingTop.scala
+++ server/app/honor/RankingTop.scala
@@ -18,8 +18,7 @@ object RankingTop extends HonorCategory {
       RankingType.Admiral.rankings.map(it => it -> it.rankingQuery(20))(breakOut)
     val admiralName = Admiral.find(memberId).map(_.nickname)
     val tops = rankings.filter { case (admiral, xs) =>
-      val top = xs.head.num
-      xs.takeWhile(_.num == top).exists(x => admiralName.contains(x.name))
+      xs.headOption.exists(top => xs.takeWhile(_.num == top.num).exists(x => admiralName.contains(x.name)))
     }.keys
     val ins = rankings.filter { case (_, xs) => xs.exists(x => admiralName.contains(x.name)) }.keys
     (tops.map(top => s"${top.title}トップ") ++ ins.map(in => s"${in.title}ランクイン")).toList
