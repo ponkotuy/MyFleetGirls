1afa13d3fed52917871fd2dfbb72b9ba6e012d42
ponkotuy <web@ponkotuy.com>
2015-03-20 01:04:42 +0900

    Add married honor

--- server/app/honor/Married.scala
+++ server/app/honor/Married.scala
@@ -1,6 +1,6 @@
 package honor
 
-import models.db.Ship
+import models.db.{MasterShipBase, Ship}
 import ranking.EvolutionBase
 import scalikejdbc._
 
@@ -15,9 +15,12 @@ object Married extends HonorCategory {
   override def approved(memberId: Long): List[String] = {
     val marrieds = Ship.findAllBy(sqls.eq(Ship.column.memberId, memberId).and.ge(Ship.column.lv, 100)).map(_.shipId)
     val married = marrieds.size
-    val marriedDist = marrieds.map(EvolutionBase(_)).distinct.size
+    val marriedDist = marrieds.map(EvolutionBase(_)).distinct
+    val marriedOne= if(marriedDist.size == 1) MasterShipBase.find(marriedDist.head) else None
     List(
-      if(marriedDist == 1) Some("一途") else None,
+      if(marriedDist.size == 1) Some("一途") else None,
+      marriedOne.map(s => s"${s.name}に一途"),
+      if(marriedDist.size == 1 && married >= 2) marriedOne.map(s => s"${s.name}の園") else None,
       if(married >= 2) Some("重婚カッコカリ") else None,
       if(married >= 10) Some("ハーレムカッコガチ") else None
     ).flatten
