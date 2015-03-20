fd935050041871d44dd8d8bbaee3899fedebe5d1
b-wind <perl.mania@gmail.com>
2015-03-20 11:29:20 +0900

    s"" 形式に修正 文字列を補完すると言う機能だったのか（知らなかった）

--- client/src/main/scala/com/ponkotuy/http/MFGHttp.scala
+++ client/src/main/scala/com/ponkotuy/http/MFGHttp.scala
@@ -36,7 +36,7 @@ object MFGHttp extends Log {
       .setRedirectsEnabled(true)
       .build()
   val httpBuilder = HttpClientBuilder.create()
-      .setUserAgent("${BuildInfo.name} client ver:${BuildInfo.version}")
+      .setUserAgent(s"${BuildInfo.name} client ver:${BuildInfo.version} / ")
       .setDefaultRequestConfig(config)
       .setSslcontext(sslContext)
 
