2d324af88d3a085c9ccd085cc912e3db85d7e368
ponkotuy <web@ponkotuy.com>
2015-03-22 15:53:17 +0900

    駆け出し提督

---
+++ server/app/honor/Rookie.scala
@@ -0,0 +1,18 @@
+package honor
+
+import models.db.Basic
+
+/**
+ *
+ * @author ponkotuy
+ * Date: 15/03/22.
+ */
+object Rookie extends HonorCategory {
+  override def category: Int = 12
+
+  override def approved(memberId: Long): List[String] = {
+    Basic.findByUser(memberId).filter(_.lv <= 50).map { _ =>
+      "駆け出し提督"
+    }.toList
+  }
+}
