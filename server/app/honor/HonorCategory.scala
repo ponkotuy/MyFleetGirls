056672060576d59d4efb66b345157d05c5c85ff5
ponkotuy <web@ponkotuy.com>
2015-03-27 02:15:05 +0900

    Add honor to lucky ship

--- server/app/honor/HonorCategory.scala
+++ server/app/honor/HonorCategory.scala
@@ -14,7 +14,7 @@ trait HonorCategory {
 }
 
 object Honors {
-  val values: Vector[HonorCategory] = Vector(ShipMaster, Yome, RankingTop, AllClear, ShipMaxLv, FleetAdmiral, Practice, ShipTypeBias, InitShip, Married, ManyShip, Rookie, FleetGroup)
+  val values: Vector[HonorCategory] = Vector(ShipMaster, Yome, RankingTop, AllClear, ShipMaxLv, FleetAdmiral, Practice, ShipTypeBias, InitShip, Married, ManyShip, Rookie, FleetGroup, Lucky)
 
   def fromUser(memberId: Long, set: Boolean = false) = {
     val where = sqls.toAndConditionOpt(
---
+++ server/app/honor/Lucky.scala
@@ -0,0 +1,23 @@
+package honor
+
+import models.db.{MasterShipBase, Ship}
+import ranking.EvolutionBase
+import scalikejdbc._
+
+/**
+ *
+ * @author ponkotuy
+ * Date: 15/03/27.
+ */
+object Lucky extends HonorCategory {
+  override def category: Int = 14
+
+  override def approved(memberId: Long): List[String] = {
+    val luckyMax = Ship.findAllWithSpec(sqls.eq(Ship.s.memberId, memberId).and.eq(Ship.mss.luckyMax, Ship.s.lucky))
+    val luckies = luckyMax.map { s => EvolutionBase(s.shipId) }
+    val withAlias = luckies ++ luckies.flatMap(EvolutionBase.Aliases.get)
+    val names = MasterShipBase.findAllBy(sqls.in(MasterShipBase.ms.id, withAlias))
+    names.map("幸運の" + _.name).distinct ++
+        (if(luckyMax.nonEmpty) Some("幸運の女神") else None)
+  }
+}
