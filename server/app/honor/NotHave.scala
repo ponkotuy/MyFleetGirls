76adfce7e92cd71c65397cc19296bb7938bebbf5
ponkotuy <web@ponkotuy.com>
2015-04-20 02:37:36 +0900

    Change NotHave honor cond

--- server/app/honor/NotHave.scala
+++ server/app/honor/NotHave.scala
@@ -1,12 +1,23 @@
 package honor
 
-import models.db.{MasterShipBase, Ship}
+import models.db.{MasterShipBase, ShipBook}
 import ranking.EvolutionBase
 import scalikejdbc._
 
 import scala.collection.breakOut
 
 /**
+ * ○○が出ないんです！って言いたいとき用称号
+ *
+ * ここのリストに入る艦娘の条件は
+ *
+ * 1. かなりレアである
+ * 2. 通常建造では出ない
+ * 3. 大型建造または通常海域のボスでのみ出現
+ *
+ * 入手がイベントのみの場合は非対象。
+ * 上記条件を満たさなくなったものは削除されるべき（だがどうせ気付かないだろうなーという本心が
+ *
  * @author ponkotuy
  * Date: 15/04/16.
  */
@@ -16,8 +27,8 @@ case object NotHave extends HonorCategory {
   override def comment: String = "持たざるものには分かる"
 
   override def approved(memberId: Long): List[String] = {
-    val ships = Ship.findAllByUser(memberId)
-    val haves: Set[Int] = ships.map { s => EvolutionBase(s.shipId) }(breakOut)
+    val shipIds = ShipBook.findAllBy(sqls.eq(ShipBook.sb.memberId, memberId)).map(_.id)
+    val haves: Set[Int] = shipIds.map(EvolutionBase(_))(breakOut)
     MasterShipBase.findAllBy(sqls.in(MasterShipBase.ms.id, (Target -- haves).toSeq)).map { ship =>
       s"${ship.name}出ない"
     }
