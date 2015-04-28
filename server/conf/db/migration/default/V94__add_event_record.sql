85cc0bc4952d1911cd1dcbc6f44fa3158175682e
ponkotuy <web@ponkotuy.com>
2015-04-29 04:30:14 +0900

    Add event data

---
+++ server/conf/db/migration/default/V94__add_event_record.sql
@@ -0,0 +1,12 @@
+
+insert into cell_info values
+    (30, 1, 1, 'A', true, false),
+    (30, 1, 2, 'B', false, false),
+    (30, 1, 3, 'C', false, false),
+    (30, 1, 4, 'D', false, false),
+    (30, 1, 6, 'F', false, false),
+    (30, 1, 8, 'H', false, true),
+    (30, 1, 9, 'H', false, true);
+
+insert into area_name values
+    (30, "'15 春 E", '発令！第十一号作戦');
