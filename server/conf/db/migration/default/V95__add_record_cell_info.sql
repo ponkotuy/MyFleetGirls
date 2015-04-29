d8bcad5f803b8176dcc0a7f4be005f83706c0b7d
ponkotuy <web@ponkotuy.com>
2015-04-29 20:34:41 +0900

    Add cell info (E-2)

---
+++ server/conf/db/migration/default/V95__add_record_cell_info.sql
@@ -0,0 +1,17 @@
+
+insert into cell_info values
+    (30, 1, 5, 'E', true, false),
+    (30, 1, 7, 'G', false, false),
+    (30, 2, 1, 'A', true, false),
+    (30, 2, 2, 'B', false, false),
+    (30, 2, 3, 'C', false, false),
+    (30, 2, 4, 'D', false, false),
+    (30, 2, 5, 'E', false, false),
+    (30, 2, 6, 'F', false, false),
+    (30, 2, 7, 'G', false, false),
+    (30, 2, 8, 'H', false, false),
+    (30, 2, 10, 'J', false, false),
+    (30, 2, 11, 'K', false, true),
+    (30, 2, 12, 'E', false, false),
+    (30, 2, 13, 'E', false, false),
+    (30, 2, 14, 'G', false, false);
