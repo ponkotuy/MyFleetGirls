4f8d33ebcfa452bba1f684214d4b00c4f8e2ec33
ponkotuy <web@ponkotuy.com>
2015-05-09 00:35:49 +0900

    Fix battle_score update

--- client/src/main/scala/com/ponkotuy/restype/ResType.scala
+++ client/src/main/scala/com/ponkotuy/restype/ResType.scala
@@ -41,33 +41,6 @@ object ResType extends Log {
   val ReqRanking = s"$Api/api_req_ranking"
   val ReqCombined = s"$Api/api_req_combined_battle"
 
-  /*
-  case object Record extends ResType(s"\\A$GetMember/record\\z".r) // Basicの綺麗版
-  case object UseItem extends ResType(s"\\A$GetMember/useitem\\z".r) // 家具箱とか
-  case object Practice extends ResType(s"\\A$GetMember/practice\\z".r) // 演習相手。取るべきではない
-  case object MapCell extends ResType(s"\\A$GetMember/mapcell\\z".r) // Mapの各Cellで通ったことあるかどうか
-  case object UpdateDeckName extends ResType(s"\\A$ReqMember/updatedeckname\\z".r) // DeckName変更
-  case object Charge extends ResType(s"\\A$ReqHokyu/charge\\z".r)
-  case object HenseiLock extends ResType(s"\\A$ReqHensei/lock\\z".r)
-  case object MissionStart extends ResType(s"\\A$ReqMission/start\\z".r)
-  case object KaisouPowerup extends ResType(s"\\A$ReqKaisou/powerup\\z".r)
-  case object Remodeling extends ResType(s"\\A$ReqKaisou/remodeling\\z".r)
-  case object Marriage extends ResType(s"\\A$ReqKaisou/marriage\\z".r) // ケッコンカッコカリ（はぁと
-  case object KaisouLock extends ResType(s"\\A$ReqKaisou/lock\\z".r) // 装備ロック
-  case object PracticeBattle extends ResType(s"\\A$ReqPractice/battle\\z".r)
-  case object PracticeMidnightBattle extends ResType(s"\\A$ReqPractice/midnight_battle\\z".r)
-  case object PracticeBattleResult extends ResType(s"\\A$ReqPractice/battle_result\\z".r)
-  case object GetOthersDeck extends ResType(s"\\A$ReqMember/getothersdeck\\z".r) // 演習相手。取るべきではない
-  case object SortieBattle extends ResType(s"\\A$ReqSortie/battle\\z".r)
-  case object ClearItemGet extends ResType(s"\\A$ReqQuest/clearitemget\\z".r)
-  case object NyukyoStart extends ResType(s"\\A$ReqNyukyo/start\\z".r)
-  case object CombinedBattleResult extends ResType(s"\\A$ReqCombined/battleresult\\z".r)
-  case object MasterPractice extends ResType(s"\\A$GetMaster/practice\\z".r)
-  case object MasterUseItem extends ResType(s"\\A$GetMaster/useitem\\z".r) // 高速修復材とかの説明
-  case object MasterFurniture extends ResType(s"\\A$GetMaster/furniture\\z".r) // 家具の説明
-  case object MasterMapArea extends ResType(s"\\A$GetMaster/maparea\\z".r) // 鎮守府海域・南西諸島海域など
-*/
-
   val values: Set[ResType] = Set(
     ApiStart2,
     Basic,
@@ -88,6 +61,7 @@ object ResType extends Log {
     RemodelSlot,
     RemodelSlotlist,
     RemodelSlotlistDetail,
+    RankingList,
     Ship2,
     Ship3,
     ShipSWF,
