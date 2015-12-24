905eb7a42cd653b6c4a14943604d1c238f1090b4
ponkotuy <web@ponkotuy.com>
2015-12-24 21:35:35 +0900

    Fix 1-6 BattleScore

--- server/app/tool/BattleScore.scala
+++ server/app/tool/BattleScore.scala
@@ -1,6 +1,6 @@
 package tool
 
-import models.db.{CellInfo, BattleResult, Basic}
+import models.db.{MapRoute, CellInfo, BattleResult, Basic}
 import com.github.nscala_time.time.Imports._
 import models.join.Stage
 import scalikejdbc._
@@ -48,47 +48,72 @@ object BattleScore {
 
   private def calcEo(memberId: Long, interval: Interval): Int = {
     StageInfo.values.map { info =>
-      val count = clearCount(memberId, info.stage, interval)
+      val count = if(info.boss) {
+        clearCountFromBattle(memberId, info.stage, interval)
+      } else {
+        clearCountFromRoute(memberId, info.stage, interval)
+      }
       if (info.clear <= count) info.score else 0
     }.sum
   }
 
-  private def clearCount(memberId: Long, stage: Stage, interval: Interval): Long = {
-    val ci = CellInfo.ci
-    val cell = CellInfo.findAllBy(sqls.eq(ci.areaId, stage.area).and.eq(ci.infoNo, stage.info))
-    val bosses = cell.filter(_.boss).map(_.cell)
+  private def clearCountFromBattle(memberId: Long, stage: Stage, interval: Interval): Long = {
     val br = BattleResult.br
+    val bosses = findBossCells(stage)
     BattleResult.countBy(
       sqls.eq(br.memberId, memberId)
           .and.eq(br.areaId, stage.area)
           .and.eq(br.infoNo, stage.info)
           .and.in(br.cell, bosses)
-          .and.gt(br.created, interval.startMillis).and.lt(br.created, interval.endMillis)
+          .and.in(br.winRank, Seq("S", "A", "B")) // 厳密には勝利しただけでカウントは進まないが、終わったのに終わってないと判断される方が怖いのでSABまで入れる
+          .and(intervalToSQLSyntax(br.created, interval))
     )
   }
 
-  private def monthHead(now: DateTime): DateTime = {
-    new LocalDate(now.getYear, now.getMonthOfYear, 1).toDateTime(LocalTime.MIDNIGHT)
+  private def clearCountFromRoute(memberId: Long, stage: Stage, interval: Interval): Long = {
+    val mr = MapRoute.mr
+    val bosses = findBossCells(stage)
+    MapRoute.countBy(
+      sqls.eq(mr.memberId, memberId)
+          .and.eq(mr.areaId, stage.area)
+          .and.eq(mr.infoNo, stage.info)
+          .and.in(mr.dest, bosses)
+          .and(intervalToSQLSyntax(mr.created, interval))
+    )
   }
 
-  private def yearHead(now: DateTime): DateTime = {
-    new LocalDate(now.getYear, 1, 1).toDateTime(LocalTime.MIDNIGHT)
+  private def findBossCells(stage: Stage): Seq[Int] = {
+    val ci = CellInfo.ci
+    val cell = CellInfo.findAllBy(sqls.eq(ci.areaId, stage.area).and.eq(ci.infoNo, stage.info))
+    cell.filter(_.boss).map(_.cell)
   }
+
+  private def monthHead(now: DateTime): DateTime =
+    new LocalDate(now.getYear, now.getMonthOfYear, 1).toDateTime(LocalTime.MIDNIGHT)
+  private def yearHead(now: DateTime): DateTime =
+    new LocalDate(now.getYear, 1, 1).toDateTime(LocalTime.MIDNIGHT)
+
+  /**
+    * BIGINTカラムの範囲指定を生成
+    */
+  private def intervalToSQLSyntax(column: SQLSyntax, interval: Interval): SQLSyntax =
+    sqls.gt(column, interval.startMillis).and.lt(column, interval.endMillis)
 }
 
 /**
   * @param score Clear score
   * @param clear Required clear count
+  * @param boss Is exists boss
   */
-case class StageInfo(stage: Stage, score: Int, clear: Int)
+case class StageInfo(stage: Stage, score: Int, clear: Int, boss: Boolean)
 
 object StageInfo {
   val values = Vector(
-    StageInfo(Stage(1, 5), 75, 4),
-    StageInfo(Stage(1, 6), 75, 7),
-    StageInfo(Stage(2, 5), 100, 4),
-    StageInfo(Stage(3, 5), 150, 4),
-    StageInfo(Stage(4, 5), 180, 5),
-    StageInfo(Stage(5, 5), 200, 5)
+    StageInfo(Stage(1, 5), 75, 4, true),
+    StageInfo(Stage(1, 6), 75, 7, false),
+    StageInfo(Stage(2, 5), 100, 4, true),
+    StageInfo(Stage(3, 5), 150, 4, true),
+    StageInfo(Stage(4, 5), 180, 5, true),
+    StageInfo(Stage(5, 5), 200, 5, true)
   )
 }
