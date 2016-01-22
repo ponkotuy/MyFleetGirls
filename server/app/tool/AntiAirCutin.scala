981cba32ce6f71c367f72311847697f218a00995
ponkotuy <web@ponkotuy.com>
2016-01-23 01:57:39 +0900

    Fix & refactor AntiAirCutin

--- server/app/tool/AntiAirCutin.scala
+++ server/app/tool/AntiAirCutin.scala
@@ -10,67 +10,76 @@ trait AntiAirCutin {
   def stName: String
   def slotMaster: Seq[MasterSlotItem]
 
-  lazy val antiAirCutin: Int = {
-    if(isMayaMk2Cutin) {
-      if(ids.exists(AntiAirRadar.contains)) 8 else 6
-    }
-    else if(akidukiType > 0) akidukiType
-    else if(isIsuzuMk2Cutin) 3
-    else if(isBattleshipCutin) {
-      if(ids.exists(AntiAirRadar.contains)) 6 else 4
-    }
-    else {
-      val hasWithGun = ids.exists(SystemWithGun.contains)
-      val hasRadar = ids.exists(AntiAirRadar.contains)
-      val countCaliber = categories.count(_ == EquipType.AntiAirGun) // 対空機銃
-      val hasGun = icons.contains(EquipIconType.AntiAirGun) // 高角砲
-      val hasSystem = ids.exists(AntiAirSystem.contains)
-      val hasManyGun = ids.exists(ManyAntiAirGun.contains)
-      if(hasWithGun && hasRadar) 4
-      else if(hasGun && hasSystem && hasRadar) 3
-      else if(hasManyGun && 2 <= countCaliber && hasRadar) 3
-      else if(hasGun && hasSystem) 2
-      else 0
+  lazy val antiAirCutin: Int = antiAirCutinOpt.getOrElse(0)
+
+  def antiAirCutinOpt: Option[Int] =
+    mayaMk2Cutin
+        .orElse(akidukiType)
+        .orElse(isuzuMk2Cutin)
+        .orElse(battleshipCutin)
+        .orElse(commonCutin)
+
+  private def mayaMk2Cutin: Option[Int] = {
+    if(shipId != MayaMk2) return None
+    if(countGun > 0 && hasManyGun) {
+      Some(if (hasAntiAirRadar) 8 else 6)
+    } else None
+  }
+
+  private def akidukiType: Option[Int] = {
+    if(!AkidukiType.contains(shipId)) return None
+    countGun match {
+      case 2 if hasRadar => Some(7)
+      case 1 if hasRadar => Some(6)
+      case 3 | 2 => Some(4)
+      case _ => None
     }
   }
 
-  def antiAirCutinOpt: Option[Int] = if(antiAirCutin == 0) None else Some(antiAirCutin)
-
-  private def ids = slotMaster.map(_.id)
-  private def categories = slotMaster.flatMap(_.category)
-  private def icons = slotMaster.flatMap(_.iconType)
-
-  private def isMayaMk2Cutin: Boolean =
-    shipId == MayaMk2 &&
-        icons.contains(EquipIconType.AntiAirGun) && // 高角砲
-        ids.exists(ManyAntiAirGun.contains)
-
-  private def akidukiType: Int = {
-    if(!AkidukiType.contains(shipId)) return 0
-    val gunCount = icons.count(_ == EquipIconType.AntiAirGun)
-    val isRadar = categories.exists(EquipType.Radars.contains(_))
-    gunCount match {
-      case 2 if isRadar => 7
-      case 1 if isRadar => 6
-      case 3 => 4
-      case 2 => 4
-      case _ => 0
-    }
+  private def isuzuMk2Cutin: Option[Int] = {
+    if(shipId != IsuzuMk2) return None
+    if(countGun > 0 && countCaliber > 0) {
+      Some(if(hasAntiAirRadar) 4 else 3)
+    } else None
   }
 
-  private def isIsuzuMk2Cutin: Boolean =
-    shipId == IsuzuMk2 &&
-        icons.contains(EquipIconType.AntiAirGun) && // 高角砲
-        (
-          categories.contains(EquipType.AntiAirGun) && // 対空機銃
-          ids.exists(AntiAirRadar.contains)
-        ) || ids.exists(AntiAirSystem.contains)
+  private def battleshipCutin: Option[Int] = {
+    if(!Battleship.contains(stName)) return None
+    if(hasMainGun && hasAntiAirBullet && hasSystem) {
+      Some(if(hasAntiAirRadar) 6 else 4)
+    } else None
+  }
 
-  private def isBattleshipCutin: Boolean =
-    Battleship.contains(stName) &&
-        categories.contains(EquipType.MainGunL) &&
-        categories.contains(EquipType.AntiAirBullet) &&
-        ids.exists(AntiAirSystem.contains)
+  private def commonCutin: Option[Int] = {
+    if(hasWithGun && hasAntiAirRadar) Some(4)
+    else if(countGun > 0 && hasSystem && hasAntiAirRadar) Some(3)
+    else if(hasManyGun && 2 <= countCaliber && hasAntiAirRadar) Some(3)
+    else if(countGun > 0 && hasSystem) Some(2)
+    else None
+  }
+
+  private lazy val ids = slotMaster.map(_.id)
+  private lazy val categories = slotMaster.flatMap(_.category)
+  private lazy val icons = slotMaster.flatMap(_.iconType)
+
+  /** 対空機銃 */
+  private lazy val countCaliber = categories.count(_ == EquipType.AntiAirGun)
+  /** 高角砲 */
+  private lazy val countGun = icons.count(_ == EquipIconType.AntiAirGun)
+  /** レーダー */
+  private lazy val hasRadar = categories.exists(EquipType.Radars.contains(_))
+  /** 対空レーダー */
+  private lazy val hasAntiAirRadar = ids.exists(AntiAirRadar.contains)
+  /** 集中配備機銃 */
+  private lazy val hasManyGun = ids.exists(ManyAntiAirGun.contains)
+  /** 高射装置 */
+  private lazy val hasSystem = ids.exists(AntiAirSystem.contains)
+  /** 高角砲+高射装置 */
+  private lazy val hasWithGun = ids.exists(SystemWithGun.contains)
+  /** 大口径主砲 */
+  private lazy val hasMainGun = categories.contains(EquipType.MainGunL)
+  /** 三式弾 */
+  private lazy val hasAntiAirBullet = categories.contains(EquipType.AntiAirBullet)
 }
 
 object AntiAirCutin {
@@ -79,7 +88,7 @@ object AntiAirCutin {
   val AkidukiType = Set(Akiduki, Teruduki, AkidukiMk1, TerudukiMk1) // 秋月型
   val ManyAntiAirGun = Set(131) // 25mm三連装機銃 集中配備など
   val AntiAirRadar = Set(27, 30, 32, 89, 106) // 対空電探
-  val SystemWithGun = Set(122, 130) // 高角砲 + 高射装置
-  val AntiAirSystem = Set(120, 121, 122, 130) // 高射装置を含む
+  val SystemWithGun = Set(122, 130, 135) // 高角砲 + 高射装置
+  val AntiAirSystem = Set(120, 121, 122, 130, 135) // 高射装置を含む
   val Battleship = Set("戦艦", "航空戦艦")
 }
