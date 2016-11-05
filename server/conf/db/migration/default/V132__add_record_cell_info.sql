a0937dbe9d4469e283211c7a6023c9f24ebce165
ponkotuy <web@ponkotuy.com>
2016-11-05 21:35:15 +0900

    Add 6-5 record_cell

---
+++ server/conf/db/migration/default/V132__add_record_cell_info.sql
@@ -0,0 +1,16 @@
+
+insert into cell_info values
+    (6, 5, 1, 'A', true, false),
+    (6, 5, 2, 'B', true, false),
+    (6, 5, 3, 'C', false, false),
+    (6, 5, 4, 'D', false, false),
+    (6, 5, 5, 'E', false, false),
+    (6, 5, 6, 'F', false, false),
+    (6, 5, 7, 'G', false, false),
+    (6, 5, 9, 'I', false, false),
+    (6, 5, 10, 'J', false, false),
+    (6, 5, 13, 'M', false, true),
+    (6, 5, 15, 'G', false, false),
+    (6, 5, 16, 'H', false, false),
+    (6, 5, 17, 'I', false, false),
+    (6, 5, 18, 'M', false, true);
