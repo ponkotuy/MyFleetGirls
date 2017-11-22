6c2bbd43e93261a5fa6ad3db911ab2cbd56b9670
ponkotuy <web@ponkotuy.com>
2017-11-22 23:15:18 +0900

    Add 2017 autumn event data (rest)

---
+++ server/conf/db/migration/default/V149__expand_cell_info_alphabet.sql
@@ -0,0 +1,1 @@
+ALTER TABLE cell_info MODIFY alphabet char(4) NOT NULL;
---
+++ server/conf/db/migration/default/V150__add_cell_info.sql
@@ -0,0 +1,14 @@
+insert into cell_info values
+  (40, 4, 38, 'Z1', false, false),
+  (40, 4, 39, 'Z2', false, false),
+  (40, 4, 40, 'Z3', false, false),
+  (40, 4, 41, 'Z4', false, false),
+  (40, 4, 42, 'Z5', false, false),
+  (40, 4, 43, 'Z6', false, false),
+  (40, 4, 44, 'Z7', false, false),
+  (40, 4, 45, 'Z8', false, false),
+  (40, 4, 46, 'Z9', false, false),
+  (40, 4, 47, 'ZZ1', false, false),
+  (40, 4, 48, 'ZZ2', false, false),
+  (40, 4, 49, 'ZZ3', false, true),
+  (40, 4, 50, 'ZZ1', false, false);
